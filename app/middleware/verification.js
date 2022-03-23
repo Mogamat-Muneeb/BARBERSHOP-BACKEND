d1fb0c084dca2a7b8d11ca5a30c4ec8f494c6180
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-03-23 20:00:56 +0200

    new

--- app/middleware/verification.js
+++ app/middleware/verification.js
@@ -4,8 +4,8 @@ const verifyEmail = (req, res, next) => {
     Customer.findOne({
         email: req.body.email
     }).exec((err, customer) => {
-        if (err) return res.status(500).send({message: 'Email already in use'});
-        if (customer) return res.status(400).send({message: 'Email already in use'});
+        if (err) return res.status(500).send({message: err.message});
+        if (customer) return res.status(400).send({message: 'email already in use'});
         next();
     })
 }
