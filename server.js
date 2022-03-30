4c0813f3deefd8a913169f09e90666e1e331e067
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-03-30 12:22:47 +0200

    root mesage

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Barbershop Database'))
 
+
+
 app.use(express.json())
 app.use(cors())
-
+app.get("/", (req, res) => {
+    res.json({ message: "welcome to the Urban"})
+})
 
 app.use('/customers', customerRouter)
 app.use('/barbers', barberRouter)
